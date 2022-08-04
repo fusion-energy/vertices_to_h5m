2d143949fff513676f578d979cacad8603517759
Jonathan Shimwell <mail@jshimwell.com>
2022-08-04 14:43:16 +0100

    list of triangles

--- examples/one_volume_cadquery_tesselation.py
+++ examples/one_volume_cadquery_tesselation.py
@@ -4,11 +4,14 @@ from vertices_to_h5m import vertices_to_h5m
 
 result = cq.Workplane("front").box(2.0, 2.0, 0.5)
 
+# the inbuild cadquery tessellate does not quite do what is needed for multi
+# parts geometry but appears to work for single part geometry.
+# The cad_to_dagmc package will provide a modified tessellate function in the future
 vertices, triangles = result.val().tessellate(tolerance=0.1)
 
 vertices_to_h5m(
     vertices=vertices,
-    triangles=triangles,
+    triangles=[triangles],
     material_tags=["mat1"],
     h5m_filename="one_cadquery_volume.h5m",
 )
